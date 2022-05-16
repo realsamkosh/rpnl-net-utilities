c20116ea19c0cb4510a2d507781296998d899e88
Akosile Samuel <happybayo@yahoo.com>
2022-05-16 17:40:29 +0100

    -Base64 Helper to Detect file extension in base64string

---
+++ rpnl.net.utilities/ImageUtils/Base64Helper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpnl.net.utilities.ImageUtils
+{
+    public class Base64Helper
+    {
+        public static string GetFileExtension(string base64String)
+        {
+            var data = base64String.Substring(0, 5);
+
+            switch (data.ToUpper())
+            {
+                case "IVBOR":
+                    return "png";
+                case "/9J/4":
+                    return "jpg";
+                case "AAAAF":
+                    return "mp4";
+                case "JVBER":
+                    return "pdf";
+                case "AAABA":
+                    return "ico";
+                case "UMFYI":
+                    return "rar";
+                case "E1XYD":
+                    return "rtf";
+                case "U1PKC":
+                    return "txt";
+                case "MQOWM":
+                case "77U/M":
+                    return "srt";
+			    case "UklGR":
+                    return "wav";
+				case "SUQzB":
+                    return "mp3";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}

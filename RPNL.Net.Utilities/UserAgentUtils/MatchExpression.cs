5612b9242ac5049c0e0c4fceb9af8ee94a8eae18
Akosile Samuel <happybayo@yahoo.com>
2022-05-20 21:52:17 +0100

    User Agent  Utils

---
+++ RPNL.Net.Utilities/UserAgentUtils/MatchExpression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RPNL.Net.Utilities.UserAgent
+{
+    public class MatchExpression
+    {
+        public List<Regex> Regexes { get; set; }
+
+        public Action<System.Text.RegularExpressions.Match, object> Action { get; set; }
+    }
+}
---
+++ RPNL.Net.Utilities/UserAgentUtils/UserAgentModel.cs
@@ -0,0 +1,38 @@
+﻿namespace RPNL.Net.Utilities.UserAgent
+{
+    public class UserAgentModel
+    {
+        private string _userAgent;
+
+        private ClientBrowser _browser;
+        public ClientBrowser Browser
+        {
+            get
+            {
+                if (_browser == null)
+                {
+                    _browser = new ClientBrowser(_userAgent);
+                }
+                return _browser;
+            }
+        }
+
+        private ClientOS _os;
+        public ClientOS OS
+        {
+            get
+            {
+                if (_os == null)
+                {
+                    _os = new ClientOS(_userAgent);
+                }
+                return _os;
+            }
+        }
+
+        public UserAgentModel(string userAgent)
+        {
+            _userAgent = userAgent;
+        }
+    }
+}

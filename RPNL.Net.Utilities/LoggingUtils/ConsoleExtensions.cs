0ade8c71b54461e012faa21036503d125990ec83
Akosile Samuel <happybayo@yahoo.com>
2023-06-15 23:02:01 +0100

    remove obsolete methods

---
+++ RPNL.Net.Utilities/LoggingUtils/ConsoleExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPNL.Net.Utilities.LoggingUtils
+{
+    public static class ConsoleExtensions
+    {
+        /// <summary>
+        /// Print error in console
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="tag"></param>
+        public static void PrintInConsole(this object @object, string tag = default, params object[] data)
+            => ConsolePrintHelper.PrintInConsole(statement: @object, tag: tag, data: data);
+
+        /// <summary>
+        /// Print error in console
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="tag"></param>
+        public static void PrintInConsole(this string text, string tag = default, params object[] data)
+            => ConsolePrintHelper.PrintInConsole(statement: text, tag: tag, data: data);
+    }
+}
